0a449194e2014c1a43e144ebbad826826509d51c
Magdalena <madz.niemiec@gmail.com>
2023-10-07 17:07:17 +0200

    Set up base_test.py

---
+++ Tests/base_test.py
@@ -0,0 +1,25 @@
+import unittest
+from selenium import webdriver
+import time
+
+
+URL = "https://opensource-demo.orangehrmlive.com/"
+
+
+class BaseTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get(URL)
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_mock(self):
+        driver = self.driver
+        driver.get(URL)
+        time.sleep(3)
+
+
+if __name__ == "__main__":
+    unittest.main()
