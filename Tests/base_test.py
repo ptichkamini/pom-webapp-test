98f25953c9658652d128e2af26ecc2dafc394609
Magdalena <madz.niemiec@gmail.com>
2023-10-10 14:11:30 +0200

    Add options to run tests in full-screen mode

--- Tests/base_test.py
+++ Tests/base_test.py
@@ -5,9 +5,10 @@ from selenium import webdriver
 class BaseTest(unittest.TestCase):
 
     def setUp(self, url="https://bearstore-testsite.smartbear.com/"):
-        self.driver = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        options.add_argument("--start-maximized")
+        self.driver = webdriver.Chrome(chrome_options=options)
         self.driver.get(url)
-
 
     def tearDown(self):
         self.driver.quit()
--- Tests/test_login.py
+++ Tests/test_login.py
@@ -1,11 +1,21 @@
 from Tests.base_test import BaseTest
 import time
 from selenium.webdriver.common.by import By
+from Resources.users import *
 
 class LoginTest(BaseTest):
 
     def test_mock(self):
+        login = self.driver.find_element(By.ID, "menubar-my-account")
         time.sleep(3)
-        login = self.driver.find_element(By.ID, "shopbar-user")
         login.click()
-        time.sleep(3)
+        user_name = self.driver.find_element(By.ID, "UsernameOrEmail")
+        user_name.send_keys(user1_login)
+        password = self.driver.find_element(By.ID, "Password")
+        password.send_keys(user1_passwort)
+        submit_login = self.driver.find_element(By.CLASS_NAME, "btn-login")
+        submit_login.click()
+        search_text = user1_login.upper()
+        print(login.text)
+
+
