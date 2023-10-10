aec990af28b8708823eee1212b8c8f64f0ce5bfe
Magdalena <madz.niemiec@gmail.com>
2023-10-10 16:16:37 +0200

    Change mock test to a basic login test

--- Tests/test_login.py
+++ Tests/test_login.py
@@ -5,9 +5,8 @@ from Resources.users import *
 
 class LoginTest(BaseTest):
 
-    def test_mock(self):
+    def test_login(self):
         login = self.driver.find_element(By.ID, "menubar-my-account")
-        time.sleep(3)
         login.click()
         user_name = self.driver.find_element(By.ID, "UsernameOrEmail")
         user_name.send_keys(user1_login)
@@ -15,7 +14,5 @@ class LoginTest(BaseTest):
         password.send_keys(user1_passwort)
         submit_login = self.driver.find_element(By.CLASS_NAME, "btn-login")
         submit_login.click()
-        search_text = user1_login.upper()
-        print(login.text)
 
 
