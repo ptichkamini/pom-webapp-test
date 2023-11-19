3d484bba3bfa862385d8e2977f9825a588728d11
Magdalena <madz.niemiec@gmail.com>
2023-11-19 16:11:29 +0100

    Write search for item

--- Pages/home_page.py
+++ Pages/home_page.py
@@ -9,6 +9,7 @@ class HomePage(BasePage):
     CART_ELEMENT = (By.ID, "shopbar-cart")
     CHECKOUT_BUTTON = (By.CLASS_NAME, "btn-action")
     SEARCH_BAR = (By.XPATH, ' //*[@id="header"]/div[2]/div/div/div[1]/div[2]/form/input')
+    SEARCH_BUTTON = (By.XPATH, '//*[@id="header"]/div[2]/div/div/div[1]/div[2]/form/button/i')
 
     def nav_to_login(self):
         self.find(self. LOGIN_ELEMENT).click()
@@ -23,3 +24,4 @@ class HomePage(BasePage):
 
     def search_for_item(self, item):
         self.find(self.SEARCH_BAR).send_keys(item)
+        self.find(self.SEARCH_BUTTON).click()
